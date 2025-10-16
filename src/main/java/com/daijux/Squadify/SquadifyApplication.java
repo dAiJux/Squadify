cc05fd0804cd0d13962853bbd4b0e5acfefcdfe6
dAiJux <arthurjoye0701@gmail.com>
2025-10-16 22:11:23 +0200

    Initiate Squadify project

---
+++ src/main/java/com/daijux/Squadify/SquadifyApplication.java
@@ -0,0 +1,13 @@
+package com.daijux.Squadify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SquadifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SquadifyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/daijux/Squadify/SquadifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daijux.Squadify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SquadifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
